cc8ab40d2a8c1e5c2d034a3fe0ba3790e224302e
Lucass5g <desenvolverbg@gmail.com>
2021-08-19 09:31:11 -0300

    tratando os dados que podem ser enviados.

--- api/Serializable.js
+++ api/Serializable.js
@@ -5,12 +5,37 @@ class Serializable {
 
   serialize(dados) {
     if (this.contentType === "application/json") {
-      return this.json(dados);
+      return this.json(this.filter(dados));
     }
 
     throw new Error(
       'Não é possível serializar o tipo de conteúdo "' + this.contentType + '".'
     );
+  }
+
+  filterObject(dados) {
+    const newObject = {};
+    const publicFields = ["id", "empresa", "categoria"];
+
+    this.publicFields.forEach((field) => {
+      if (dados.hasOwnProperty(field)) {
+        newObject[field] = dados[field];
+      }
+    });
+
+    return newObject;
+  }
+
+  filter(dados) {
+    if (Array.isArray(dados)) {
+      dados = dados.map((item) => {
+        return this.filterObject(item);
+      });
+    } else {
+      dados = this.filterObject(dados);
+    }
+
+    return dados;
   }
 }
 
@@ -18,6 +43,7 @@ class SupplierSerializable extends Serializable {
   constructor(contentType) {
     super();
     this.contentType = contentType;
+    this.publicFields = ["id", "empresa", "categoria"];
   }
 }
 
